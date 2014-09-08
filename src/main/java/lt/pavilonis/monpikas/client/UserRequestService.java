a952209a64ca44299f726655b6a9983b724fc517
Arturas Pavilonis <arturas.pavilonis@gmail.com>
2014-09-09 00:44:24 +0300

    'User Update Queue' now works in ViewController. Added UserRequestService. Added check to avoid requesting new User if animation not finished.

--- src/main/java/lt/pavilonis/monpikas/client/UserRequestService.java
+++ src/main/java/lt/pavilonis/monpikas/client/UserRequestService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class FirstService {
+public class UserRequestService {
 
    @Autowired
    private RestTemplate rt;
